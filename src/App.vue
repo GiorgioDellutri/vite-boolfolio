175a7c26eb677aac225fc8b6fd294a6da7fcb0e8
GiorgioDellutri <giorgiodll93@gmail.com>
2023-03-06 23:36:54 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script>
+import AppMain from './components/AppMain.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AppMain,
+  },
+  data() {
+    return {
+      // dati del componente 
+    }
+  }
+}
+</script>
+
+<template>
+  <main>
+    <AppMain />
+  </main>
+</template>
+
+<style lang="scss">
+@use './styles/general.scss' as *;
+@use 'node_modules/bootstrap/scss/bootstrap' as *;
+
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,55 @@
+<template lang="">
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h2>
+                        Posts:
+                    </h2>                 
+                </div>
+                <div class="col-6 single-post" v-for="post in posts">
+                    {{post.title}}
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'AppMain',
+    data() {
+        return {
+            posts: [],
+            loading: false,
+            urlAddress: 'http://127.0.0.1:8000/api/post',
+        }
+    },
+
+    methods: {
+        getPosts() {
+            axios.get(this.urlAddress, {
+                params: {
+                }
+            })
+                .then((response) => {
+                    this.posts = response.data.results;
+                    console.log(this.posts)
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
+    },
+
+
+    created() {
+        this.getPosts();
+    },
+
+}
+</script>
+
+<style scoped lang="scss"></style>
